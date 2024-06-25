517d99ed83c637d14e8919dc54300cd55a842570
Punyapat Lertsamrong <punyapat1412@gmail.com>
2024-06-25 14:31:36 +0700

    add version and global prefix

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,6 +1,9 @@
-import { Controller,Get,HttpCode,Post,Req } from '@nestjs/common';
+import { Controller,Get,HttpCode,Post,Req, Version } from '@nestjs/common';
 import { Request } from 'express';
-@Controller('customers')
+@Controller({
+    path:'customers',
+    version:'1',
+})
 export class CustomersController {
     @Get()
     @HttpCode(201)
@@ -11,5 +14,12 @@ export class CustomersController {
     @HttpCode(200)
     create(@Req() request:Request): string {
         return request.body;
+    }
+
+    @Get()
+    @Version('2')
+    @HttpCode(201)
+    findAll2(): string {
+        return 'This action returns all customers v2';
     }
 }
